c6ba5511cb6be4bfce384b62a1dc001c97630cba
SiHyung2 <153695533+SiHyung2@users.noreply.github.com>
2024-01-16 17:58:04 +0900

    Add files via upload

--- homework_2nd/BasicGui.java
+++ homework_2nd/BasicGui.java
@@ -1,53 +1,62 @@
 package homework_2nd;
 
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Label;
+import java.awt.TextArea;
+import java.awt.TextField;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class BasicGui extends JFrame{
 	private static final long serialVersionUID = -6336499109619045510L;
-	public BasicGui() {
-		setTitle("Person Manager");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c=getContentPane();
-		 setLayout(new GridLayout(3, 1));  // 3행 1열
-		c.setLayout(null);
-		setSize(400,300);
-		setVisible(true);
-		
-		//첫번째 줄 :라벨, 텍스트 필드, 버튼
-		JPanel firstPanel=new JPanel(new FlowLayout());
-		JLabel label=new JLabel("파일 경로 : ");   //라벨 생성
-		JTextField textField=new JTextField(10);
-		JButton button=new JButton("열기");
-		
-		firstPanel.add(label);
-		firstPanel.add(textField);
-		firstPanel.add(button);
-		
-		
-		//두번째 줄: 버튼
-		JButton b2=new JButton("추가");  
-		c.add(b2);
-		JButton b3=new JButton("수정");   
-		c.add(b3);
-		JButton b4=new JButton("삭제");  
-		c.add(b4);
-		JButton b5=new JButton("정렬");  
-		c.add(b5);
-		JButton b6=new JButton("저장");   
-		c.add(b6);
-		
-		
-		
-	}
-	public static void main(String[] args) {
-		new BasicGui();
-	}
+	GridBagLayout gBag;  
+    public BasicGui() {
+        //삽입할 컴포넌트 생성.
+        Label lblReceive = new Label("받을 사람:",Label.RIGHT);  
+        Label lblContent = new Label("내 용:",Label.RIGHT);
+        Label lblFile = new Label("첨부 파일:",Label.RIGHT);
+        Button btnSearch = new Button("찾아보기");
+        Button btnSend = new Button("보내기");
+        TextField toPerson = new TextField(40);     
+        TextField file = new TextField(30);
+        TextArea content = new TextArea(5,40);
+        gBag = new GridBagLayout();    // 현재 클래스를 그리드백(grid bag) 레이아웃으로 설정
+       
+        setLayout(gBag);
+        gbinsert(lblReceive, 0, 0, 1, 1); //GridBagLayout에 삽입
+        gbinsert(toPerson, 1, 0, 3, 1);    
+        gbinsert(lblContent, 0, 1, 1, 1);
+        gbinsert(content, 1, 1, 3, 1);
+        gbinsert(lblFile, 0, 2, 1, 1);
+        gbinsert(file, 1, 2, 2, 1);
+        gbinsert(btnSearch, 3, 2, 1, 1);
+        gbinsert(btnSend, 0, 3, 4, 1);
+       
+       
+        this.pack();   //윈도우 크기를 자동으로 설정
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+   
+
+    // 그리드 레이아웃 매니저를 사용할 때 컴포넌트의 제약 조건 정의하는 클래스
+    public void gbinsert(Component c, int x, int y, int w, int h){
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill= GridBagConstraints.BOTH;      
+        gbc.gridx = x;      
+        gbc.gridy = y;
+        gbc.gridwidth = w;     
+        gbc.gridheight = h;
+        gBag.setConstraints(c,gbc);       
+        this.add(c);   
+       
+       
+    }
+   
+    public static void main(String[] args) {
+        new BasicGui();
+    }
 }
--- homework_2nd/tset1.java
+++ homework_2nd/tset1.java
@@ -12,18 +12,19 @@ import javax.swing.JFrame;
 
 public class tset1 extends JFrame{
 	private static final long serialVersionUID = 7821144668532475480L;
-	GridBagLayout gBag;
+	GridBagLayout gBag;   //그리드 레이아웃 객체 생성.   다른 클래스에 그리드 적용할지도 모르니 클래스 변수로 생성됨
     public tset1() {
         //삽입할 컴포넌트 생성.
-        Label lblReceive = new Label("받을 사람:",Label.RIGHT);
+        Label lblReceive = new Label("받을 사람:",Label.RIGHT);   //".RIGHT" 부컴포넌트의 정렬 속성인데 큰 의미는 없음
         Label lblContent = new Label("내 용:",Label.RIGHT);
         Label lblFile = new Label("첨부 파일:",Label.RIGHT);
         Button btnSearch = new Button("찾아보기");
         Button btnSend = new Button("보내기");
-        TextField toPerson = new TextField(40);
-        TextField file = new TextField(30);
-        TextArea content = new TextArea(5,40);
-        gBag = new GridBagLayout(); //GridBagLayout 생성
+        TextField toPerson = new TextField(40);     //단일 행의 텍스트 필드.  최대 글자 수는 40자
+        TextField file = new TextField(30);       //두번째 텍스트 필드.
+        TextArea content = new TextArea(5,40);     //여러줄을 입력 할 수 있는 텍스트 영역이다. 5행 40열로 지정됨
+        gBag = new GridBagLayout();    // 현재 클래스를 그리드백 레이아웃 매니저 설정
+        				//그리드백 레이아웃 : 그리드 레이아웃 중 하나. 다양한 크기의 그리드 셀을 배치하고 유연하게 배치 제어 할 수 있다 
        
         setLayout(gBag);
         gbinsert(lblReceive, 0, 0, 1, 1); //GridBagLayout에 삽입 , 모눈종이에 컴포넌트를 배치한다고 생각
@@ -36,20 +37,26 @@ public class tset1 extends JFrame{
         gbinsert(btnSend, 0, 3, 4, 1);
        
        
-        this.pack();
+        this.pack();   //윈도우 크기를 자동으로 설정
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
    
+    
+    //매개변수에 적혀있는 Component 는 자바 AWT 패키지의 클래스의 최상위 클래스이다
+    //모든 사용자 인터페이스 요소의 기본 클래스로 사용됨
+    // 그리드 레이아웃 매니저를 사용할 때 컴포넌트의 제약 조건 정의하는 클래스이다
+    //GridBagConstraints 를 호출하여 배치와 크기를 지정할 수 있었다
     public void gbinsert(Component c, int x, int y, int w, int h){
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill= GridBagConstraints.BOTH;
-        gbc.gridx = x;
+        gbc.fill= GridBagConstraints.BOTH;       //객체 생성,   BOTH는 가로 및 세로로 그리드 셀을 채우도록 지정
+        gbc.gridx = x;        //왼쪽 상단의 x,y 좌표
         gbc.gridy = y;
-        gbc.gridwidth = w;
+        gbc.gridwidth = w;      //가로크기와 세로크기 지정
         gbc.gridheight = h;
-        gBag.setConstraints(c,gbc);
-        this.add(c);
+        gBag.setConstraints(c,gbc);   //그리드백 레이아웃에 컴포넌트의 제약조건 설정 (제약조건에 따라 위치와 크기로 추가됨)
+                               //메소드에서 클래스 변수로 생성된 객체에 전달됨에 유의
+        this.add(c);   //현재 클래스의 컨테이너에 컴포넌트 추가
        
        
     }
