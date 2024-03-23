1c709cdd704473883b47e111abc14dcb8bdb8213
SiHyung2 <153695533+SiHyung2@users.noreply.github.com>
2024-03-23 15:26:58 +0900

    Add files via upload

---
+++ java/com/example/config/RootConfig.java
@@ -0,0 +1,45 @@
+package com.example.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+
+//import javax.activation.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages={"com.example.service"})
+@MapperScan(basePackages= {"com.example.mapper"})
+public class RootConfig {
+	 @Bean
+	  public HikariDataSource dataSource() 
+	  {
+	    HikariConfig hikariConfig = new HikariConfig();
+	    
+//	    hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+//	    hikariConfig.setDriverClassName("oracle.jdbc.OracleDriver");
+	    hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+	    hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1522:XE");
+
+	    hikariConfig.setUsername("c##madang");
+	    hikariConfig.setPassword("c##madang");
+
+	    HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+	    return dataSource;
+	  }
+	 
+	 @Bean
+	 public SqlSessionFactory sqlSessionFactory() throws Exception{
+		 SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		 sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	 }
+	 
+}
